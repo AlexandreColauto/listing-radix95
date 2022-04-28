5a684eac2ba20667a69e0b6e579e3cb2020d3755
AlexandreColauto <xandecolauto@gmail.com>
2022-04-28 17:16:01 +0100

    index fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const { authenticate } = useMoralis();
+  const { authenticate, isAuthenticated } = useMoralis();
 
   const handleClick = (e) => {
     authenticate();
   };
   return (
     <div className={styles.container}>
-      <button onClick={handleClick}>authenticate</button>
+      {!isAuthenticated ? (
+        <button onClick={handleClick}>authenticate</button>
+      ) : (
+        <>
+          <Link href={"/browser"}>
+            <button>browser</button>
+          </Link>
+          <Link href={"/listing"}>
+            <button>listing</button>
+          </Link>
+        </>
+      )}
     </div>
   );
 }
